c4aeda03a8bdab45329a161510fc31ab6659af6c
Suvash Joshi <suvash2077@gmail.com>
2023-11-21 22:25:05 +0545

    buildui

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Need to put password");
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://sushi:${password}@phonebook.5nwx7gy.mongodb.net/?retryWrites=true&w=majority`;
+
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = mongoose.Schema({
+  person: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  person: personName,
+  number: personNumber,
+});
+
+if (process.argv.length == 5) {
+  person.save().then((result) => {
+    console.log(`Added ${personName} number ${personNumber} in Phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+Person.find({}).then((result) => {
+  result.forEach((element) => {
+    console.log(element.person, element.number);
+  });
+  mongoose.connection.close();
+});
