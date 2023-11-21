4a806e1f1232117e00b774b0f0abff05875280c5
Suvash Joshi <suvash2077@gmail.com>
2023-11-21 13:37:17 +0545

    remove morgan log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 morgan.token("output", (request, response) => {
   return JSON.stringify(request.body);
