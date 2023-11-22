5a74159cd39f9fd58f44e048368a61aa39051180
Suvash Joshi <suvash2077@gmail.com>
2023-11-22 15:05:43 +0545

    buildui

--- index.js
+++ index.js
@@ -9,40 +9,58 @@ app.use(express.json());
 
 const Person = require("./models/person");
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastMessage") {
+    return response.status(400).send({ error: "Malformatted Id" });
+  }
+};
+
+app.use(errorHandler);
+
+//Homepage
 app.get("/", (request, response) => {
   response.send(
     "This is the homepage. Go to /api/persons to find the persons database."
   );
 });
 
+//Get list of person in the phonebook
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((person) => {
     response.json(person);
   });
 });
 
+//Info page
 app.get("/info", (request, response) => {
   response.send(
-    `<p>Phonebook has info for ${Object.keys(persons).length} people.</p>
+    `<p>Phonebook has info for ${Object.keys(Person).length} people.</p>
     <br/>
     ${Date()}
     `
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) response.send(person);
-  else response.status(404).end();
+//Getting individual person
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) response.json(person);
+      else console.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  response.send(persons.filter((person) => person.id !== id));
+//Deleting person
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(response.status(204).end())
+    .catch((error) => next(error));
 });
 
+//Adding a person
 app.post("/api/persons/", (request, response) => {
   const body = request.body;
 
@@ -58,6 +76,22 @@ app.post("/api/persons/", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
+});
+
+//Updaiting phone number
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;
